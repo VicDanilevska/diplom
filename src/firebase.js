209406cc17bbf0aa1e52dd398a6c1fcecdd659f0
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-22 11:13:15 +0300

    Ініціалізовано підключення у firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9Q4k0OEaxE4GPl8ZhetwfkfT0P6gOejs",
+    authDomain: "my-diplom-10e3f.firebaseapp.com",
+    projectId: "my-diplom-10e3f",
+    storageBucket: "my-diplom-10e3f.appspot.com",
+    messagingSenderId: "889973681865",
+    appId: "1:889973681865:web:9882d1f488a4ae23a6e718"
+};
+
+const app = initializeApp(firebaseConfig);
