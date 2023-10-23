19552faa9289dd253ae17cba99292a6ef2ca96af
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-23 11:25:34 +0300

    Створено темплейт сторінки

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -2,9 +2,45 @@ import React from 'react';
 
 const LogIn = () => {
     return (
-        <div>
+        <motion.section
+            initial={{x: '100%'}}
+            animate={{x: 0, transition: {duration: 0.5}}}
+            exit={{x: '-100%', transition: {duration: 0.5}}}
+            className={'flex flex-col justify-center items-center h-screen w-full overflow-x-hidden'}>
+
+            <div className={''}>
+                <form action="" className={''}>
+                    <div className={''}>
+                        <label htmlFor="" className={''}>
+
+                        </label>
+                        <input type="text" className={''}/>
+                    </div>
+                    <div className={''}>
+                        <label htmlFor="" className={''}>
+
+                        </label>
+                        <input type="text" className={''}/>
+
+                    </div>
+                    <div className={''}>
+                        <button className={''}>
+
+                            <div className="">
+
+                            </div>
+                        </button>
+                        <Link className="">
+
+                        </Link>
+
+                    </div>
+                </form>
+
+            </div>
+
             
-        </div>
+        </motion.section>
     );
 };
 
