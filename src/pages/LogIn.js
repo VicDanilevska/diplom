cfe0ac8e888700fdd3966c8c2f6b289fb2b5182b
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-23 15:33:54 +0300

    Стилізовано навігаційні посилання на головну , або у аккаунт , якщо авторизований користувач

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -48,14 +48,14 @@ const LogIn = () => {
 
             <div className={'w-2/6'}>
 
-                <div>
-                    <Link to={`/list`} className={''}>
-                        <FaHome className={''}></FaHome>
+                <div className={'flex'}>
+                    <Link to={`/list`} className={'transition-all duration-250 group hover:bg-blue-600 grow border border-t-blue-500 w-1/2 flex justify-center items-center px-4 py-2'}>
+                        <FaHome className={'transition-all duration-250 group-hover:text-white text-6xl text-blue-700'}></FaHome>
                     </Link>
                     {user
                         &&
-                    <Link to={`/users/${user.uid}`} className={''}>
-                        <FaUser className={''}></FaUser>
+                    <Link to={`/users/${user.uid}`} className={'transition-all duration-250 group hover:bg-blue-600 grow border border-t-blue-500 w-1/2 flex justify-center items-center px-4 py-2'}>
+                        <FaUser className={'transition-all duration-250 group-hover:text-white text-6xl text-blue-700'}></FaUser>
                     </Link>}
                 </div>
 
