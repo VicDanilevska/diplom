b7f5a87fb3f23a17192cbf9dbb82495b26314a01
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-23 14:55:45 +0300

    Стилізовано та анімовано модальне вікно

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,14 +5,35 @@ import {BiError, BiCheck} from "react-icons/bi";
 
 const Modal = ({text, isError}) => {
     return (
-        <motion.div className={''}>
-            <h1 className={''}>
+        <motion.div
+            initial={{
+                opacity: 0,
+                x: '100%',
+            }}
+            animate={{
+                opacity: 1,
+                x: '0',
+                transition: {
+                    duration: 0.5,
+                    type: 'ease-in-out'
+                },
+            }}
+            exit={{
+                opacity: 0,
+                x: '-100%',
+                transition: {
+                    duration: 0.5,
+                    type: 'ease-in-out'
+                },
+            }}
+            className={`absolute top-20 border shadow-md ${isError ? 'shadow-red-400' : 'shadow-green-400'} px-4 py-2 ${isError ? 'border-red-400' : 'border-green-400'} bg-white flex`}>
+            <h1 className={'text-2xl'}>
                 {text}
             </h1>
             {isError ?
-                <BiError className={''}/>
+                <BiError className={'text-4xl text-red-500 ml-2'}/>
                 :
-                <BiCheck className={''}/>
+                <BiCheck className={'text-4xl text-green-500 ml-2'}/>
             }
         </motion.div>
     );
