083d94e49a7534111c62660f7abfdbd4828bcabb
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-24 15:12:16 +0300

    Створено базовий темплейт сторінки аккаунту

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,10 +1,82 @@
 import React from 'react';
+import {AnimatePresence} from "framer-motion";
+import {Link} from "react-router-dom";
+import {useUserContext} from "../context/UserContextProvider";
 
 const Account = () => {
+    const user = useUserContext();
     return (
-        <div>
-            
-        </div>
+        <motion.section
+            exit={{x: '-100%', transition: {duration: 0.5}}}
+            className={`flex flex-row justify-end h-screen`}>
+            <motion.div
+                className={'w-2/5 flex flex-col gap-5 h-full py-4 shadow-md shadow-blue-400 fixed left-0 top-0'}
+                initial={{
+                    y: '100%'
+                }}
+                animate={{
+                    y: '0',
+                    transition: {
+                        duration: 0.5
+                    }
+                }}>
+
+                <div className={''}>
+                    <img src={} alt="" className={''}/>
+                    <h1 className={''}></h1>
+                </div>
+
+                <div className={''}>
+
+                    <AnimatePresence initial={true} mode={'wait'}>
+
+                        <motion.img
+                            key={}
+                            src={}
+                            alt=""
+                            className={''}
+                        />
+                    </AnimatePresence>
+
+                </div>
+
+                <div className={''}>
+
+                </div>
+            </motion.div>
+            <div className={''}>
+                {
+                    (user && id === user.uid)
+                    &&
+                    <Link to={'/edit'}
+                          className={''}>Редагувати
+                        сторінку</Link>
+                }
+                <motion.div
+
+                    className={''}>
+                    <h1 className={''}>Про нас</h1>
+
+                </motion.div>
+
+                <motion.div
+                    className={''}>
+                    <h1 className={''}>Короткий опис</h1>
+                </motion.div>
+
+
+                <motion.div
+
+                    className={''}>
+                    <h1 className={''}>Наші послуги</h1>
+                    <div className={''}>
+
+                    </div>
+                </motion.div>
+
+            </div>
+
+        </motion.section>
     );
 };
 
