2a452e8816223bb3bdf0638acf363d601a968552
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-26 13:54:47 +0300

    Додано завантаження даних послуг для поточного користувача, та обробка оновлення інформації послуги в реальному часі

--- src/context/UserContextProvider.js
+++ src/context/UserContextProvider.js
@@ -1,7 +1,7 @@
 import React, {createContext, useContext, useEffect, useState} from 'react';
 import {onAuthStateChanged} from 'firebase/auth';
-import {auth, db} from '../firebase';
-import {getDoc, doc} from 'firebase/firestore';
+import {auth, db, servicesCollection} from '../firebase';
+import {getDoc, doc, query, where, getDocs, onSnapshot} from 'firebase/firestore';
 import PreLoader from "../components/PreLoader";
 
 const UserContext = createContext(null);
@@ -13,17 +13,46 @@ const UserContextProvider = ({children}) => {
         onAuthStateChanged(auth, user => {
             if (user) {
                 const id = user.uid;
+                const serviceQ = query(servicesCollection, where('owner', '==', id));
 
                 const docRef = doc(db, 'users', id);
                 getDoc(docRef).then(doc => {
                     setUser(() => ({...user, data: doc?.data()}))
                 })
 
+                getDocs(serviceQ).then(({docs}) => {
+                    setUser((prev) => ({
+                        ...prev, data: {
+                            ...(prev.data),
+                            services: docs.map(doc => ({...doc?.data(), id: doc.id}))
+                        }
+                    }))
+                })
             } else {
                 setUser(user);
             }
         })
     }, [])
+
+    useEffect(() => {
+        if (user){
+            onSnapshot(doc(db, 'users', user.uid), doc => {
+                const id = user.uid;
+                const serviceQ = query(servicesCollection, where('owner', '==', id));
+
+                setUser(() => ({...user, data: doc?.data()}))
+
+                getDocs(serviceQ).then(({docs}) => {
+                    setUser((prev) => ({
+                        ...prev, data: {
+                            ...(prev.data),
+                            services: docs.map(doc => ({...doc?.data(), id: doc.id}))
+                        }
+                    }))
+                })
+            })
+        }
+    }, [user?.uid])
 
     return (
         <UserContext.Provider value={user}>
