8966d3ceb1a0363648f0977e52cde5c39416394a
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-26 10:47:30 +0300

    Додано отримання списку послуг, що належать певному користувачу , за id, і додано їх до загальної дати користувача

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {doc, getDoc} from "firebase/firestore";
-import {db} from "../firebase";
+import {doc, getDoc, getDocs, query, where} from "firebase/firestore";
+import {db, servicesCollection} from "../firebase";
 
 const UseFetch = (id = null) => {
     const [data, setData] = useState({});
@@ -8,11 +8,18 @@ const UseFetch = (id = null) => {
     const [error, setError] = useState(null);
     useEffect(() => {
         const docRef = doc(db, 'users', id);
+        const serviceQ = query(servicesCollection, where('owner', '==', id));
 
         getDoc(docRef).then(doc => {
             setData(doc.data())
         }).catch(error => {
             setError(error)
+        })
+
+        getDocs(serviceQ).then(({docs}) => {
+            setData((prev) => ({...prev, services: docs.map(doc => ({...doc?.data(), id: doc.id}))}))
+        }).catch((error) => {
+            setError(error);
         }).finally(() => {
             setIsFetching(false);
         })
