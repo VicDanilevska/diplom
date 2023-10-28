68889afe309d1309b6799af9f9028d08dbfd0bd3
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-28 23:23:37 +0300

    Виправлена помилка з відсутністю протоколу у посиланні

--- src/components/MediaLink.js
+++ src/components/MediaLink.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MediaLink = ({href, className, icon}) => {
     return (
         <div className={`hover:grow rounded-2xl py-2 px-4 ${className} relative flex items-center justify-center cursor-pointer transition-all duration-500`}>
-            <a href={href} className={'text-4xl text-white rounded-xl z-10'}>{icon}</a>
+            <a href={href.startsWith('https://') ? href : 'https://' + href} target={'_blank'} className={'text-4xl text-white rounded-xl z-10'}>{icon}</a>
         </div>
     );
 };
