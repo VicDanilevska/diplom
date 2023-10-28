e9286d09dc2fa0e4c79ed7d03248a6017d900a5a
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-28 15:43:22 +0300

    Для головної стрічки , створено та стилізовано пошукову строку , без реалізованої логіки

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {motion} from "framer-motion";
+import {AiOutlineSearch} from 'react-icons/ai';
 
 const List = () => {
+
     return (
-        <div>
-            
-        </div>
+        <motion.section
+            initial={{x: '100%'}}
+            animate={{x: 0, transition: {duration: 0.5}}}
+            exit={{x: '-100%', transition: {duration: 0.5}}}
+            className={'flex flex-col items-center h-screen w-full overflow-x-hidden'}>
+
+            <div className={'w-3/4 mt-20 relative flex items-center'}>
+                <input
+                    type="text"
+                    className={'pl-5 pr-14 py-2 w-full rounded-full shadow-inner shadow-blue-600 text-4xl border border-blue-400'}
+                    placeholder={'Введіть назву компанії'}/>
+                <AiOutlineSearch className={'absolute right-5 text-2xl text-blue-700 rounded-full'}/>
+            </div>
+
+        </motion.section>
     );
 };
 
