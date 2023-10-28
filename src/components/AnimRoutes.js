e7d8641aa55e24ad6f3342045a9d545469a9aead
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-28 15:00:51 +0300

    Додана переадресація на сторінку Логіну якщо користувач не авторизований і намагається зайти на сторінку редагування

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {AnimatePresence} from "framer-motion";
-import {Route, Routes, useLocation} from "react-router-dom";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom";
 import Home from "../pages/Home";
 import LogIn from "../pages/LogIn";
 import SignUp from "../pages/SignUp";
 import Account from "../pages/Account";
 import List from "../pages/List";
 import Edit from "../pages/Edit";
+import {useUserContext} from "../context/UserContextProvider";
 
 const AnimRoutes = () => {
 
     const location = useLocation();
+    const user = useUserContext();
 
     return (
         <AnimatePresence initial={true} mode={'wait'}>
@@ -20,7 +22,7 @@ const AnimRoutes = () => {
                 <Route path={'/signup'} element={<SignUp/>}></Route>
                 <Route path={'/users/:id'} element={<Account/>}></Route>
                 <Route path={'/list'} element={<List/>}></Route>
-                <Route path={'/edit'} element={<Edit/>}></Route>
+                <Route path={'/edit'} element={user !== null ? <Edit/> : <Navigate replace to={'/login'}/>}></Route>
             </Routes>
         </AnimatePresence>
     );
