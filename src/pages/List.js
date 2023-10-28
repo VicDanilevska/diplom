474a5b9a43325fceb3c7cc07f32e474662edbf8a
Victoria Danilevska <danilevska.viktoriia1619@vu.cdu.edu.ua>
2023-10-28 16:42:19 +0300

    Додано завантаження та збереження усіх користувачів , та додано пре лоадер

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {motion} from "framer-motion";
 import {AiOutlineSearch} from 'react-icons/ai';
+import useFetchAll from "../hooks/useFetchAll";
+import PreLoader from "../components/PreLoader";
 
 const List = () => {
 
+    const {users, isFetching} = useFetchAll();
+
     return (
-        <motion.section
-            initial={{x: '100%'}}
-            animate={{x: 0, transition: {duration: 0.5}}}
-            exit={{x: '-100%', transition: {duration: 0.5}}}
-            className={'flex flex-col items-center h-screen w-full overflow-x-hidden'}>
+        isFetching ?
+            <PreLoader/>
+            :
+            <motion.section
+                initial={{x: '100%'}}
+                animate={{x: 0, transition: {duration: 0.5}}}
+                exit={{x: '-100%', transition: {duration: 0.5}}}
+                className={'flex flex-col items-center h-screen w-full overflow-x-hidden'}>
 
-            <div className={'w-3/4 mt-20 relative flex items-center'}>
-                <input
-                    type="text"
-                    className={'pl-5 pr-14 py-2 w-full rounded-full shadow-inner shadow-blue-600 text-4xl border border-blue-400'}
-                    placeholder={'Введіть назву компанії'}/>
-                <AiOutlineSearch className={'absolute right-5 text-2xl text-blue-700 rounded-full'}/>
-            </div>
+                <div className={'w-3/4 mt-20 relative flex items-center'}>
+                    <input
+                        type="text"
+                        className={'pl-5 pr-14 py-2 w-full rounded-full shadow-inner shadow-blue-600 text-4xl border border-blue-400'}
+                        placeholder={'Введіть назву компанії'}/>
+                    <AiOutlineSearch className={'absolute right-5 text-2xl text-blue-700 rounded-full'}/>
+                </div>
 
-        </motion.section>
+            </motion.section>
     );
 };
 
